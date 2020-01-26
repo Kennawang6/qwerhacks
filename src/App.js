f829dcb96c27c0161b5bae373e7701113d5a597d
Kenna Wang <kenna.wang6@gmail.com>
2020-01-26 01:09:34 -0800

    Deleted stuff from App.js
    Image also works now

--- src/App.js
+++ src/App.js
@@ -13,33 +13,8 @@ class App extends React.Component {
   render = () => {
       return (
           <div className="app-container">
-            <img src="../images/MacBook-1.png" alt='Welcome to QWER Films'/>
+            <img src="https://i.imgur.com/ew9MqKP.png" alt="text"/>
             <SearchBar />
-              <header className="header-text">the hive</header>
-              <image src="https://i.imgur.com/ew9MqKP.png"></image>
-              <p>find out what's all the buzz!</p>
-              <hr />
-              <div className="message-box">
-                  <input
-                      className="text-input"
-                      type="text"
-                      value={this.state.author}
-                      onChange={this.handleAuthorChange}
-                  />
-                  <input
-                      className="text-input"
-                      type="text"
-                      value={this.state.message}
-                      onChange={this.handleMessageChange}
-                  />
-                  <button
-                    className="send-button"
-                    onClick={this.createMessage}
-                  >
-                      Send Message
-                  </button>
-              </div>
-              {this.renderMessages()}
           </div>
       );
   }
