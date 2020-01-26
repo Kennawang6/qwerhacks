a572fec0b77b24e92029b070784c4719714003e4
Kenna Wang <kenna.wang6@gmail.com>
2020-01-26 00:11:55 -0800

    Began SearchBar

---
+++ src/components/Filter.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+class Filter extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      lower: 0,
+      upper: 0,
+      name: "choose filter"
+    }
+  }
+
+  handleLowerChange = e => {
+    this.setState({lower: e.target.value})
+  }
+
+  handleUpperChange = e => {
+    this.setState({upper: e.target.value})
+  }
+
+    render(){
+        return (
+            <div className="filter">
+                <div className="filter-item">
+                    <p><b>{this.props.name}</b> </p>
+                    <button className="dropbtn">name</button>
+                    <input
+                      className="text-input"
+                      type="number"
+                      value={this.state.lower}
+                      onChange={this.handleLowerChange}
+                    /> to
+                    <input
+                      className="text-input"
+                      type="number"
+                      value={this.state.upper}
+                      onChange={this.handleUpperChange}
+                    />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Filter;
